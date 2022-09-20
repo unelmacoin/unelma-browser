e7ce2e879e854b698f86115efbb97b37ce516e96
AhmedMuhammed7 <27medltanani@gmail.com>
2022-09-20 14:09:26 +0200

    fix decryption method

--- src/main/controllers/passwords.js
+++ src/main/controllers/passwords.js
@@ -4,23 +4,23 @@ const store = new Store();
 
 module.exports = {
   getAuthInfo: () => {
-    const authInfo = store.get("auth-info") || [];
+    const authInfo = store.get("auth") || [];
     return authInfo.map((info) => ({
       ...info,
       password: decrypt(info.password),
     }));
   },
   addAuthInfo: (info) => {
-    let authInfo = store.get("auth-info") || [];
+    let authInfo = store.get("auth") || [];
     if (!authInfo.find(({ id }) => id === info.id)) {
       authInfo.push({ ...info, password: encrypt(info.password) });
     }
-    store.set("auth-info", authInfo);
+    store.set("auth", authInfo);
   },
   removeFromAuthInfo: (id) => {
-    let authInfo = store.get("auth-info") || [];
+    let authInfo = store.get("auth") || [];
     store.set(
-      "auth-info",
+      "auth",
       authInfo.filter((item) => item.id !== id)
     );
   },
