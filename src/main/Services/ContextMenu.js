0f6488a936d091cc89101587b89c5dc575cf0799
JESSE <jessejzee@gmail.com>
2023-07-07 01:27:44 +0300

    image saved . working on the default file name

--- src/main/Services/ContextMenu.js
+++ src/main/Services/ContextMenu.js
@@ -1,5 +1,9 @@
 const { dialog } = require("electron");
 const contextMenu = require("electron-context-menu");
+const { app } = require('electron');
+const path = require('path');
+const { net } = require('electron');
+const fs = require("fs");
  class ContextMenu {
   constructor(contents, addWindow) {
     contextMenu({
@@ -18,12 +22,37 @@ const contextMenu = require("electron-context-menu");
           click: () => {
             dialog.showSaveDialog(browserWindow, {
               title: 'Save As',
-              defaultPath: '~/'
-            })
+              defaultPath: path.join(app.getPath('home'), (parameters.title || 'image') + '.png'),
+            }).then(result => {
+              if (!result.canceled) {
+                const url = parameters.srcURL;
+                const filePath = result.filePath;
+                download(url, filePath);
+              }
+            });
           }
         },
       ]
+      
+ 
     });
+  }
+}
+
+
+function download(url, filePath) {
+  if (url.startsWith('data:')) {
+    const base64Data = url.split(',')[1];
+    const buffer = Buffer.from(base64Data, 'base64');
+    fs.writeFileSync(filePath, buffer);
+  } else {
+    const request = net.request(url);
+    request.on('response', (response) => {
+      response.on('data', (chunk) => {
+        fs.appendFileSync(filePath, chunk);
+      });
+    });
+    request.end();
   }
 }
 
