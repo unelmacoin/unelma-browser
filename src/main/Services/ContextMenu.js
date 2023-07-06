c91206a592e20a939004187a047b3460ae76359a
JESSE <jessejzee@gmail.com>
2023-07-06 14:48:31 +0300

    UP-32 enable save as dialog for image

--- src/main/Services/ContextMenu.js
+++ src/main/Services/ContextMenu.js
@@ -1,16 +1,28 @@
+const { dialog } = require("electron");
 const contextMenu = require("electron-context-menu");
  class ContextMenu {
   constructor(contents, addWindow) {
     contextMenu({
       window: contents,
       showInspectElement: true,
-      showSaveImageAs:true,
       prepend: () => [
         {
           label: "New window",
           click: () => addWindow(),
         },
       ],
+      prepend: (defaultActions, parameters, browserWindow) => [
+        {
+          label: 'Save Image As ...',
+          visible: parameters.mediaType === 'image',
+          click: () => {
+            dialog.showSaveDialog(browserWindow, {
+              title: 'Save As',
+              defaultPath: '~/'
+            })
+          }
+        },
+      ]
     });
   }
 }
