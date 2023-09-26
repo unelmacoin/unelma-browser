217632a4c688bed0fbd577a306cea6a1f98266bc
Vijay KC <vj.kc2007@gmail.com>
2023-09-26 14:54:36 +0300

    Get help redirection to support fixed

--- src/main/Services/TopBarMenu.js
+++ src/main/Services/TopBarMenu.js
@@ -1,101 +1,97 @@
-const { app } = require('electron');
-const isMac = process.platform === 'darwin'
+const { app } = require("electron");
+import { UNELMA_SUPPORT_URL } from "../../constants/global/urls";
+import { MainWindow } from "./MainWindow";
+
+const isMac = process.platform === "darwin";
+let mainWindow;
 
 export const template = [
   ...(isMac
-    ? [{
-        label: app.name,
-        submenu: [
-          { role: 'about' },
-          { type: 'separator' },
-          { role: 'services' },
-          { type: 'separator' },
-          { role: 'hide' },
-          { role: 'hideOthers' },
-          { role: 'unhide' },
-          { type: 'separator' },
-          { role: 'quit' }
-        ]
-      }]
+    ? [
+        {
+          label: app.name,
+          submenu: [
+            { role: "about" },
+            { type: "separator" },
+            { role: "services" },
+            { type: "separator" },
+            { role: "hide" },
+            { role: "hideOthers" },
+            { role: "unhide" },
+            { type: "separator" },
+            { role: "quit" },
+          ],
+        },
+      ]
     : []),
   {
-    label: 'File',
-    submenu: [
-      isMac ? { role: 'close' } : { role: 'quit' }
-    ]
+    label: "File",
+    submenu: [isMac ? { role: "close" } : { role: "quit" }],
   },
   {
-    label: 'Edit',
+    label: "Edit",
     submenu: [
-      { role: 'undo' },
-      { role: 'redo' },
-      { type: 'separator' },
-      { role: 'cut' },
-      { role: 'copy' },
-      { role: 'paste' },
+      { role: "undo" },
+      { role: "redo" },
+      { type: "separator" },
+      { role: "cut" },
+      { role: "copy" },
+      { role: "paste" },
       ...(isMac
         ? [
-            { role: 'pasteAndMatchStyle' },
-            { role: 'delete' },
-            { role: 'selectAll' },
-            { type: 'separator' },
+            { role: "pasteAndMatchStyle" },
+            { role: "delete" },
+            { role: "selectAll" },
+            { type: "separator" },
             {
-              label: 'Speech',
-              submenu: [
-                { role: 'startSpeaking' },
-                { role: 'stopSpeaking' }
-              ]
-            }
+              label: "Speech",
+              submenu: [{ role: "startSpeaking" }, { role: "stopSpeaking" }],
+            },
           ]
-        : [
-            { role: 'delete' },
-            { type: 'separator' },
-            { role: 'selectAll' }
-          ])
-    ]
+        : [{ role: "delete" }, { type: "separator" }, { role: "selectAll" }]),
+    ],
   },
   {
-    label: 'View',
+    label: "View",
     submenu: [
-      { role: 'reload' },
-      { role: 'forceReload' },
-      { role: 'toggleDevTools' },
-      { type: 'separator' },
-      { role: 'resetZoom' },
-      { role: 'zoomIn' },
-      { role: 'zoomOut' },
-      { type: 'separator' },
-      { role: 'togglefullscreen' }
-    ]
+      { role: "reload" },
+      { role: "forceReload" },
+      { role: "toggleDevTools" },
+      { type: "separator" },
+      { role: "resetZoom" },
+      { role: "zoomIn" },
+      { role: "zoomOut" },
+      { type: "separator" },
+      { role: "togglefullscreen" },
+    ],
   },
   {
-    label: 'Window',
+    label: "Window",
     submenu: [
-      { role: 'minimize' },
-      { role: 'zoom' },
+      { role: "minimize" },
+      { role: "zoom" },
       ...(isMac
         ? [
-            { type: 'separator' },
-            { role: 'front' },
-            { type: 'separator' },
-            { role: 'window' }
+            { type: "separator" },
+            { role: "front" },
+            { type: "separator" },
+            { role: "window" },
           ]
-        : [
-            { role: 'close' }
-          ])
-    ]
+        : [{ role: "close" }]),
+    ],
   },
   {
-    role: 'Help',
+    role: "Help",
     submenu: [
       {
-        label: 'Get Help',
-        click: async () => {
-          const { shell } = require('electron')
-          await shell.openExternal('https://unelmasupport.com')
-          
-        }
-      }
-    ]
-  }
-]
+        label: "Get Help",
+        click: () => {
+          mainWindow = new MainWindow(null, UNELMA_SUPPORT_URL)
+          app.on("ready-to-show", () => {
+            mainWindow.window.show()
+          })
+        },
+      },
+    ],
+  },
+];
