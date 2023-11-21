5ccd130792ac44fd63b0bfb7db61adb2778ba505
Anderson Martin <info@unelmaplatforms.com>
2023-11-21 08:17:10 +0200

    Update package.json

--- build/notarize.js
+++ build/notarize.js
@@ -1,30 +1,43 @@
-const { notarize } = require('electron/notarize');
+const { notarize } = require('@electron/notarize');
 const path = require('path');
 
 exports.default = async function notarizing(context) {
-  if (context.electronPlatformName !== 'darwin' || process.env.CSC_IDENTITY_AUTO_DISCOVERY === 'false') {
-    console.log("Skipping notarization");
+  if (
+    context.electronPlatformName !== 'darwin' ||
+    process.env.CSC_IDENTITY_AUTO_DISCOVERY === 'false'
+  ) {
+    console.log('Skipping notarization');
     return;
   }
-  console.log("Notarizing...")
+  console.log('Notarizing...');
 
   const appBundleId = context.packager.appInfo.info._configuration.appId;
   const appName = context.packager.appInfo.productFilename;
-  const appPath = path.normalize(path.join(context.appOutDir, `${appName}.app`));
+  const appPath = path.normalize(
+    path.join(context.appOutDir, `${appName}.app`)
+  );
   const appleId = process.env.APPLE_ID;
+  const teamId = process.env.APPLE_TEAMID;
   const appleIdPassword = process.env.APPLE_ID_PASSWORD;
   if (!appleId) {
-    console.warn("Not notarizing: Missing APPLE_ID environment variable");
+    console.warn('Not notarizing: Missing APPLE_ID environment variable');
+    return;
+  }
+  if (!teamId) {
+    console.warn('Not notarizing: Missing APPLE_TEAMID environment variable');
     return;
   }
   if (!appleIdPassword) {
-    console.warn("Not notarizing: Missing APPLE_ID_PASSWORD environment variable");
+    console.warn(
+      'Not notarizing: Missing APPLE_ID_PASSWORD environment variable'
+    );
     return;
   }
   return notarize({
     appBundleId,
     appPath,
     appleId,
+    teamId,
     appleIdPassword,
   });
 };
